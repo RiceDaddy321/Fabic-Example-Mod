88bd76a0a4a63636cd6c430417604d18b3eb10ee
RiceDaddy321 <0carlitos2@gmail.com>
2020-12-09 19:28:24 -0600

    This lays the foundation for the mod.

---
+++ src/main/java/net/fabric/example/FabricExample.java
@@ -0,0 +1,13 @@
+package net.fabric.example;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricExample implements ModInitializer    {
+
+    @Override
+    public void onInitialize() {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
